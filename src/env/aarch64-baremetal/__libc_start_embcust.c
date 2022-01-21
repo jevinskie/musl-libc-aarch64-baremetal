6c65ef867b6c3deddcf5154ed275ac04e1ea8979
Jevin Sweval <jevinsweval@gmail.com>
2022-01-21 14:45:13 -0800

    fix crap

--- src/env/aarch64-baremetal/__libc_start_embcust.c
+++ src/env/aarch64-baremetal/__libc_start_embcust.c
@@ -2,7 +2,9 @@
 #include "atomic.h"
 #include "libc.h"
 
-extern weak hidden void (*const __init_array_start)(void), (*const __init_array_end)(void);
+extern void (*const __init_array_start)(void), (*const __init_array_end)(void);
+
+const size_t *_DYNAMIC = NULL;
 
 #define EMBCUST_MAX_ARGV 32
 #define EMBCUST_MAX_ENVP EMBCUST_MAX_ARGV
@@ -11,9 +13,9 @@ extern weak hidden void (*const __init_array_start)(void), (*const __init_array_
 extern const char **__embcust_argv;
 extern const char **__embcust_environ;
 
-char *__embcust_argv_rw[EMBCUST_MAX_ARGV];
-char *__embcust_environ_rw[EMBCUST_MAX_ENVP];
-char __embcust_init_strpool[EMBCUST_MAX_INIT_STRPOOL];
+static char *__embcust_argv_rw[EMBCUST_MAX_ARGV];
+static char *__embcust_environ_rw[EMBCUST_MAX_ENVP];
+static char __embcust_init_strpool[EMBCUST_MAX_INIT_STRPOOL];
 
 
 static void libc_start_embcust_init(void)
@@ -29,21 +31,21 @@ static lsm2_fn libc_start_embcust_stage2;
 int __libc_start_embcust(void)
 {
     int argc = 0;
-    for (const char **arg = __embcust_argv; *arg; ++argc);
+    for (const char **arg = __embcust_argv; *arg; ++argc, ++arg);
     if (argc + 1 > EMBCUST_MAX_ARGV)
-        __builtin_trap();
+        a_crash();
 
     int envc = 0;
-    for (const char **env = __embcust_environ; *env; ++envc);
+    for (const char **env = __embcust_environ; *env; ++envc, ++env);
     if (envc + 1 > EMBCUST_MAX_ENVP)
-        __builtin_trap();
+        a_crash();
 
     char *strpool_end = __embcust_init_strpool + sizeof(__embcust_init_strpool);
     char *strpool = __embcust_init_strpool;
     for (int i = 0; i < argc; ++i) {
         size_t len = strlen(__embcust_argv[i]);
         if (strpool + len + 1 >= strpool_end)
-            __builtin_trap();
+            a_crash();
         memcpy(strpool, __embcust_argv[i], len + 1);
         __embcust_argv_rw[i] = strpool;
         strpool += len + 1;
@@ -52,7 +54,7 @@ int __libc_start_embcust(void)
     for (int i = 0; i < envc; ++i) {
         size_t len = strlen(__embcust_environ[i]);
         if (strpool + len + 1 >= strpool_end)
-            __builtin_trap();
+            a_crash();
         memcpy(strpool, __embcust_environ[i], len + 1);
         __embcust_environ_rw[i] = strpool;
         strpool += len + 1;
