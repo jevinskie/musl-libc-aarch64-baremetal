717fd52f3c7616b9f761f702908aad328f2eb398
Jevin Sweval <jevinsweval@gmail.com>
2022-01-23 11:24:24 -0800

    i guess i fixed __pthread_self a few commits ago. cleanup

--- arch/aarch64-baremetal/pthread_arch_post.h
+++ arch/aarch64-baremetal/pthread_arch_post.h
@@ -1,7 +1,4 @@
 static inline uintptr_t __get_tp()
 {
-    uintptr_t tp = &__builtin_tls[0].pt;
-    uintptr_t tp_adj = TP_ADJ(tp);
-    __asm__ __volatile__("nop\n\t");
-    return (uintptr_t)tp_adj;
+    return (uintptr_t)TP_ADJ(&__builtin_tls[0].pt);
 }
