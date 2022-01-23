b392a3981b54d7db173bcb44b54fc859d7c43528
Jevin Sweval <jevinsweval@gmail.com>
2022-01-23 11:21:35 -0800

    this works... self is correct in __uselocale

--- arch/aarch64-baremetal/pthread_arch_post.h
+++ arch/aarch64-baremetal/pthread_arch_post.h
@@ -1,4 +1,7 @@
 static inline uintptr_t __get_tp()
 {
-    return (uintptr_t)TP_ADJ(&__builtin_tls[0].pt);
+    uintptr_t tp = &__builtin_tls[0].pt;
+    uintptr_t tp_adj = TP_ADJ(tp);
+    __asm__ __volatile__("nop\n\t");
+    return (uintptr_t)tp_adj;
 }
