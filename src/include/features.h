e586287a40793f53bf7ea02a45e4f5d146c84476
Jevin Sweval <jevinsweval@gmail.com>
2022-11-08 10:35:36 -0500

    apple partial build fix

--- src/include/features.h
+++ src/include/features.h
@@ -5,7 +5,13 @@
 
 #define weak __attribute__((__weak__))
 #define hidden __attribute__((__visibility__("hidden")))
+#ifndef __APPLE__
 #define weak_alias(old, new) \
 	extern __typeof(old) new __attribute__((__weak__, __alias__(#old)))
-
+#else
+#define weak_alias(old, new) \
+	__asm__(".globl _" #new); \
+	__asm__("_" #new " = _" #old); \
+	extern __typeof(old) new __attribute__((weak_import))
+#endif
 #endif
