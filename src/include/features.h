b3a38ec985d4abb8b7d5ac614626474fc0993d36
Jevin Sweval <jevinsweval@gmail.com>
2022-11-18 10:13:59 -0500

    apple macho libc builds

--- src/include/features.h
+++ src/include/features.h
@@ -10,6 +10,6 @@
 	extern __typeof(old_sym) new_sym __attribute__((__weak__, __alias__(#old_sym)))
 #else
 #define weak_alias(old_sym, new_sym) \
-	extern __typeof(old_sym) new_sym __attribute__((__weak__))
+	extern __typeof(old_sym) new_sym __attribute__((__weak__, weak_import))
 #endif
 #endif
