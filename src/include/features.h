d7505ec9e2caf742d15d75ed7f06143545bb8187
Jevin Sweval <jevinsweval@gmail.com>
2022-11-18 11:16:32 -0500

    go all in

--- src/include/features.h
+++ src/include/features.h
@@ -10,6 +10,8 @@
 	extern __typeof(old_sym) new_sym __attribute__((__weak__, __alias__(#old_sym)))
 #else
 #define weak_alias(old_sym, new_sym) \
-	extern __typeof(old_sym) new_sym __attribute__((__weak__, weak_import))
+	__asm__(".weak_reference _" #old_sym); \
+	__asm__("_" #new_sym " = _" #old_sym); \
+	extern __typeof(old_sym) new_sym __attribute__((weak_import))
 #endif
 #endif
